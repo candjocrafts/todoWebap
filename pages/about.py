d82e9f4f86e512a672e1d01a77506fcbb5323c30
rhubert616 <robcmhubert@gmail.com>
2024-04-29 10:07:05 -0400

    Added about page with webcam app

--- pages/about.py
+++ pages/about.py
@@ -1,5 +1,10 @@
 import streamlit as st
+from PIL import Image
 
 st.write("hello")
 
-st.camera_input("Camera")
+img = st.camera_input("Camera")
+
+gray_img = img.convert("L")
+
+st.image(gray_img)
