6f34b9ddc6634864df28c48858a55c3452770ea4
rhubert616 <robcmhubert@gmail.com>
2024-04-29 10:25:19 -0400

    Added about page with webcam app

--- pages/about.py
+++ pages/about.py
@@ -4,8 +4,9 @@ from PIL import Image
 st.write("hello")
 
 camera_image = st.camera_input("Camera")
-img = Image.open((camera_image))
 
-gray_img = img.convert("L")
 
-st.image(gray_img)
+if camera_image:
+    img = Image.open(camera_image)
+    gray_img = img.convert("L")
+    st.image(camera_image)
